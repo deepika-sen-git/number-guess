c0c2ddaf50e52ed0408e723fa0587421e132f7d3
deepika-sen-git <147013559+deepika-sen-git@users.noreply.github.com>
2025-03-31 11:05:11 +0530

    Add files via upload

---
+++ constants/colors.js
@@ -0,0 +1,11 @@
+const Colors = {
+
+    pink: '#ff1493',
+    red: '#b22222',
+    maroon: '#800000',
+    dullYellow: '#daa520',
+    yellow: '#ffd700',
+    blue: '#00bfff',
+
+};
+export default Colors;
